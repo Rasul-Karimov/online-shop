b6258f8f85b7d840e97ff2a787a0ed58cd27e908
Rasulbek <rasulbekkari008@gmail.com>
2021-12-29 23:50:54 +0600

    slider rooms

--- js/main.js
+++ js/main.js
@@ -154,19 +154,50 @@ const swiper = new Swiper('.slider-main__wrapper', {
     preloadImages: false,
     parallax: true,
 
-    // If we need pagination
     pagination: {
-        el: '.controls-slider-main__dotta',
+        el: '.slider-main .swiper-pagination',
         clickable: true,
     },
 
-    // Navigation arrows
+    // Navigation arrows 
     navigation: {
-        nextEl: '.slider-arrow__next',
-        prevEl: '.slider-arrow__prev',
+        nextEl: '.slider-main .slider-arrow__next',
+        prevEl: '.slider-main .slider-arrow__prev',
     },
 
 
+})
+
+
+
+const swiperRooms = new Swiper('.slider-rooms__body', {
+    observer: true,
+    observeParents: true,
+    slidesPerView: "auto",
+    spaceBetween: 24,
+    watchOverflow: true,
+    speed: 800,
+    loop: true,
+    loopAdditionalSlides: 5,
+    preloadImages: false,
+    parallax: true,
+
+    pagination: {
+        el: '.slider-rooms .slider-rooms__dotts',
+        clickable: true,
+    },
+
+    // Navigation arrows 
+    navigation: {
+        nextEl: '.slider-rooms .slider-arrow__next',
+        prevEl: '.slider-rooms .slider-arrow__prev',
+    },
+
+    breakpoint: {
+        480: {
+            slidesPerView: 1,
+        }
+    }
 })
 
 
